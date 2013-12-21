1aec3a93e9f1404884651cedc7dff7fb04107232
AndyBeginIt <zhexiao@163.com>
2013-12-21 16:52:33 -0500

    add linkedin

--- socialms.sql
+++ socialms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
--- 主机: 127.0.0.1
--- 生成日期: 2013 年 12 月 20 日 22:37
--- 服务器版本: 5.5.32
--- PHP 版本: 5.4.19
+-- 主机: localhost
+-- 生成日期: 2013 年 12 月 21 日 16:51
+-- 服务器版本: 5.1.44
+-- PHP 版本: 5.3.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,8 +18,6 @@ SET time_zone = "+00:00";
 --
 -- 数据库: `socialms`
 --
-CREATE DATABASE IF NOT EXISTS `socialms` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `socialms`;
 
 -- --------------------------------------------------------
 
@@ -91,6 +88,35 @@ INSERT INTO `xz_social_instagram` (`id`, `user_id`, `instagram_userid`, `instagr
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `xz_social_linkedin`
+--
+
+CREATE TABLE IF NOT EXISTS `xz_social_linkedin` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `linkedin_access_token` varchar(250) NOT NULL,
+  `linkedin_userid` varchar(20) NOT NULL,
+  `linkedin_username` varchar(40) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
+  `is_deleted` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- 转存表中的数据 `xz_social_linkedin`
+--
+
+INSERT INTO `xz_social_linkedin` (`id`, `user_id`, `linkedin_access_token`, `linkedin_userid`, `linkedin_username`, `create_time`, `update_time`, `is_deleted`) VALUES
+(1, 15, 'AQW2rJYlMO0qgvHbTuYuqeBtVTNq8j4v9cO9F8GnPENo2e-ngneTGvPI0-GTqifaR6E-7A7IjDEuHH_2SCwyTFRmWb98RctJJQW8lK_3p5551wQPvCgW2eBny1vG4VlFujtzf1kASxVjte_6hMHmRdEzOLtTh8yHRbZACkRoRsO-21hUmCo', 'X4w47fwaaf', 'Andy Xiao', '2013-12-21 16:04:05', '2013-12-21 16:47:43', '1'),
+(2, 15, 'AQWnKG6O86W6F8kaLx4Ul3Jdf4x65isN0FUId6mGkg-G5rVakKVvo2TWv1thUyVu48aaXCBOzBCbmUChikb1frnAlAh10geC6CDMCzTBMe4ubr5iRHNyDOGv0s-bGMWcYrPIGU3_CXQhQM_U1cUAaGx7SoWNDYVqcpjUY7hb_0nQ7iRB340', 'X4w47fwaaf', 'Andy Xiao', '2013-12-21 16:23:18', '2013-12-21 16:47:51', '1'),
+(3, 15, 'AQVHfoOXajlRP4hIWELtQNv5gvaDgTY3_gWafOwjFUzCVJLCsQ1QNDaZ2n9ae6N1l5RBuLvjuRLGPGjMfWHthUgHIVIqeTfm9Mq7SQdSOk-fJVGwJjPgnSIK7sqvuNpivIofmpnPBSxYEaV8rQ5XziONOzf3EaGTzJZvjxZk7YzxWOIkodE', 'X4w47fwaaf', 'Andy Xiao', '2013-12-21 16:30:52', '2013-12-21 16:47:55', '1'),
+(4, 15, 'AQWlI-T1K2pPwzUc4BPKIFfXOEISTsezDW3LFN_6v_fRzrXt0ApPutqq4604eciIFfeECbMsdcnKfrU8P76qcjmbG4QP3wXAbRLf8S6-Plid7rKY0fS6jdXOfW6xxzaoNxON2BxDvlLfNYIB5op3yKOTQbQcqVDB5XmQISbT6J-V2NvAMzU', 'X4w47fwaaf', 'Andy Xiao', '2013-12-21 16:50:47', '2013-12-21 16:50:47', '0');
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `xz_social_pinterest`
 --
 
@@ -143,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `xz_users` (
 --
 
 INSERT INTO `xz_users` (`id`, `email`, `username`, `password`, `create_time`, `update_time`, `last_login_time`, `is_deleted`) VALUES
-(15, 'zhexiao@163.com', 'andybegin', '31efe5c727df3e9f116cd46fbb5b2626', '2013-12-16 16:57:44', '2013-12-16 16:57:44', '2013-12-20 21:09:17', '0'),
+(15, 'zhexiao@163.com', 'andybegin', '31efe5c727df3e9f116cd46fbb5b2626', '2013-12-16 16:57:44', '2013-12-16 16:57:44', '2013-12-21 16:07:20', '0'),
 (17, 'test@qq.com', 'test', '098f6bcd4621d373cade4e832627b4f6', '2013-12-16 17:03:28', '2013-12-16 17:03:28', '2013-12-16 17:03:28', '0');
 
 -- --------------------------------------------------------
@@ -180,25 +206,22 @@ CREATE TABLE IF NOT EXISTS `xz_user_column` (
   KEY `weibo_id` (`weibo_id`),
   KEY `video56_id` (`video56_id`),
   KEY `youku_id` (`youku_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=55 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=67 ;
 
 --
 -- 转存表中的数据 `xz_user_column`
 --
 
 INSERT INTO `xz_user_column` (`id`, `user_id`, `view_id`, `social_type`, `instagram_id`, `pinterest_id`, `flickr_id`, `linkedin_id`, `reddit_id`, `renren_id`, `weibo_id`, `video56_id`, `youku_id`, `create_time`, `update_time`, `is_deleted`) VALUES
-(43, 15, 2, 2, 24, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-19 20:17:29', '2013-12-19 20:20:21', '1'),
-(44, 15, 2, 2, 25, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-19 20:20:52', '2013-12-19 20:21:09', '1'),
-(45, 15, 2, 2, 25, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-19 20:20:54', '2013-12-19 20:21:08', '1'),
-(46, 15, 2, 2, 25, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-19 20:20:57', '2013-12-19 20:21:07', '1'),
-(47, 15, 2, 2, 26, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 17:32:18', '2013-12-20 17:32:18', '0'),
-(48, 15, 2, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 18:52:41', '2013-12-20 19:07:53', '1'),
-(49, 15, 2, 3, 0, 2, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 19:04:02', '2013-12-20 19:06:53', '1'),
-(50, 15, 2, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 19:07:08', '2013-12-20 19:07:52', '1'),
-(51, 15, 2, 3, 0, 4, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 19:09:01', '2013-12-20 19:09:01', '0'),
-(52, 15, 2, 4, 0, 0, 11, 0, 0, 0, 0, 0, 0, '2013-12-20 21:14:38', '2013-12-20 21:20:37', '1'),
-(53, 15, 2, 4, 0, 0, 15, 0, 0, 0, 0, 0, 0, '2013-12-20 21:20:40', '2013-12-20 22:23:08', '1'),
-(54, 15, 2, 4, 0, 0, 14, 0, 0, 0, 0, 0, 0, '2013-12-20 22:23:30', '2013-12-20 22:23:30', '0');
+(58, 15, 2, 2, 26, 0, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 20:08:01', '2013-12-20 20:08:01', '0'),
+(59, 15, 2, 3, 0, 2, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 20:08:03', '2013-12-20 20:08:03', '0'),
+(60, 15, 2, 4, 0, 0, 14, 0, 0, 0, 0, 0, 0, '2013-12-20 20:08:05', '2013-12-20 20:08:05', '0'),
+(61, 15, 2, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, '2013-12-20 20:08:11', '2013-12-21 02:17:33', '1'),
+(62, 15, 2, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, '2013-12-21 16:20:55', '2013-12-21 16:23:04', '1'),
+(63, 15, 2, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, '2013-12-21 16:23:23', '2013-12-21 16:47:51', '1'),
+(64, 15, 2, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, '2013-12-21 16:47:36', '2013-12-21 16:47:43', '1'),
+(65, 15, 2, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, '2013-12-21 16:47:37', '2013-12-21 16:47:43', '1'),
+(66, 15, 2, 5, 0, 0, 0, 4, 0, 0, 0, 0, 0, '2013-12-21 16:50:52', '2013-12-21 16:50:52', '0');
 
 -- --------------------------------------------------------
 
@@ -224,7 +247,7 @@ CREATE TABLE IF NOT EXISTS `xz_user_view` (
 
 INSERT INTO `xz_user_view` (`id`, `user_id`, `name`, `create_time`, `update_time`, `is_active`, `is_deleted`) VALUES
 (1, 3, '默认', '2013-12-16 16:54:00', '2013-12-16 16:54:00', '1', '0'),
-(2, 15, '默认', '2013-12-16 16:57:44', '2013-12-19 18:13:37', '1', '0'),
+(2, 15, '默认', '2013-12-16 16:57:44', '2013-12-20 18:43:12', '1', '0'),
 (3, 17, '默认', '2013-12-16 17:03:28', '2013-12-16 17:03:28', '1', '0'),
 (4, 15, '默认1', '2013-12-16 16:57:44', '2013-12-16 21:01:46', '0', '1'),
 (6, 15, 'asd', '2013-12-16 21:31:42', '2013-12-16 22:16:59', '0', '1'),
@@ -235,14 +258,14 @@ INSERT INTO `xz_user_view` (`id`, `user_id`, `name`, `create_time`, `update_time
 (11, 15, 'neal', '2013-12-16 21:46:37', '2013-12-16 22:15:49', '0', '1'),
 (12, 15, '111', '2013-12-16 22:14:06', '2013-12-16 22:15:41', '0', '1'),
 (13, 15, '财经', '2013-12-16 22:17:25', '2013-12-16 22:48:31', '0', '1'),
-(14, 15, '新闻', '2013-12-16 22:17:33', '2013-12-19 18:13:25', '0', '0'),
+(14, 15, '新闻', '2013-12-16 22:17:33', '2013-12-20 18:43:06', '0', '0'),
 (15, 15, '231', '2013-12-16 22:19:54', '2013-12-16 22:20:01', '0', '1'),
 (16, 15, '才才', '2013-12-16 22:49:09', '2013-12-16 22:51:35', '0', '1'),
 (17, 15, '123', '2013-12-16 23:06:44', '2013-12-16 23:07:06', '0', '1'),
 (18, 15, '123dfd', '2013-12-16 23:06:51', '2013-12-16 23:07:08', '0', '1'),
 (19, 15, '123dfdgsdfgsdfg', '2013-12-16 23:06:54', '2013-12-16 23:07:04', '0', '1'),
 (20, 15, '呵呵', '2013-12-16 23:12:57', '2013-12-16 23:13:08', '0', '1'),
-(21, 15, '呵呵', '2013-12-16 23:13:10', '2013-12-19 18:13:37', '0', '0'),
+(21, 15, '呵呵', '2013-12-16 23:13:10', '2013-12-20 18:43:12', '0', '0'),
 (22, 15, 'good', '2013-12-16 23:36:25', '2013-12-16 23:36:56', '0', '1'),
 (23, 15, 'asd', '2013-12-19 17:48:18', '2013-12-19 18:05:35', '0', '1'),
 (24, 15, 'asdasd', '2013-12-19 17:49:10', '2013-12-19 18:05:29', '0', '1'),
@@ -252,7 +275,3 @@ INSERT INTO `xz_user_view` (`id`, `user_id`, `name`, `create_time`, `update_time
 (28, 15, '啊啊啊', '2013-12-19 17:55:45', '2013-12-19 18:05:14', '0', '1'),
 (29, 15, 'asd', '2013-12-19 18:05:47', '2013-12-19 18:05:51', '0', '1'),
 (30, 15, 'asd', '2013-12-19 18:06:22', '2013-12-19 18:06:25', '0', '1');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
